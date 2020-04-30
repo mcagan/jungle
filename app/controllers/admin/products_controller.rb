ff68bf94a35cd271c04ec277d63e44c522646d44
mcagan <meliscagan@gmail.com>
2020-04-30 21:53:04 +0000

    implement basic user authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,6 @@
 class Admin::ProductsController < ApplicationController
     http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PWD']
+    before_filter :authorize
 
   def index
     @products = Product.order(id: :desc).all
