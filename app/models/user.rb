ca76627f442ff71a020d26a90de34f7f5f725739
mcagan <meliscagan@gmail.com>
2020-05-05 18:40:49 +0000

    implement user authentication class method and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, uniqueness: { case_sensitive: false }
   validates :name, presence: true
-  validates :password, length: {minimum: 8}
+  validates :password, length: {minimum: 8}, confirmation: true
+  validates :password_confirmation, presence: true
+
+
+  def self.authenticate_with_credentials(email, password)
+    @user = User.find_by_email(email.strip.downcase)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
   setup do
     @attrs = { email: "something@here.com", password: "12345678", password_confirmation: "12345678", name: "Michelle"}
   end
-   describe 'Validations' do
+  describe 'Validations' do
     it "checks that password and password confirmation are the same" do
       @user = User.new({name: "Michelle", email: "test@test.com", password: "StrongPassword123", password_confirmation: "NotStrong"})
       expect(@user).to_not be_valid
@@ -35,6 +35,36 @@ RSpec.describe User, type: :model do
     it "validates that password has at least 8 characters" do
       @user = User.new({email: "test@test.com", password: "Strong", password_confirmation: "Strong"})
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it "should log the user in if the correct email and password are entered" do
+      @user = User.create!(name:"bob", email: 'bob@example.com', password: 'password12', password_confirmation: 'password12')
+      user = User.authenticate_with_credentials('bob@example.com', 'password12')
+      expect(user).to eq(@user)
+    end
+
+    it "should make sure that the correct email and password are entered" do
+      @user = User.create!({name: "Michelle", email: "test@test.com", password: "StrongPassword123", password_confirmation: "StrongPassword123"})
+      user = User.authenticate_with_credentials("test@test.com", "notcorrect")
+      expect(user).to be_nil
+    end
+
+    it "should sign in the user even if the email has extra spaces" do
+      @user = User.create!({name: "Michelle", email: "test@test.com", password: "StrongPassword123", password_confirmation: "StrongPassword123"})
+      @email = "     test@test.com"
+      @password = "StrongPassword123"
+      user = User.authenticate_with_credentials(@email, @password)
+      expect(user).to eq(@user)
+    end
+
+    it "should sign in the user even if the email has wrong case" do
+      @user = User.create!({name: "Michelle", email: "test@test.com", password: "StrongPassword123", password_confirmation: "StrongPassword123"})
+      @email = "TEST@test.Com"
+      @password = "StrongPassword123"
+      user = User.authenticate_with_credentials(@email, @password)
+      expect(user).to eq(@user)
     end
   end
 end
