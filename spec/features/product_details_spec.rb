b5247a25d51132bf1a7db24f8aa9099e82656ee2
mcagan <meliscagan@gmail.com>
2020-05-06 20:23:10 +0000

    implement product detail page spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,31 +1,35 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.feature "ProductDetails", type: :feature, js: true do
+RSpec.feature "ProductDetails", type: :feature, js: true do
 
-#   # SETUP
-#   before :each do
-#     @category = Category.create! name: 'Apparel'
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
 
-#     10.times do |n|
-#       @category.products.create!(
-#         name:  Faker::Hipster.sentence(3),
-#         description: Faker::Hipster.paragraph(4),
-#         image: open_asset('apparel1.jpg'),
-#         quantity: 10,
-#         price: 64.99
-#       )
-#     end
-#   end
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
 
-#   scenario "They see all products" do
-#     # ACT
-#     visit root_path
+  scenario "They can visit a product detail page" do
+    p = Product.all.sample
+    # ACT
+    visit root_path
+    expect(page).to have_text(p.name)
+    link = page.find(:xpath, ".//a[@href='/products/#{p.id}']", text: p.name)
+    link.trigger('click')
 
+    # DEBUG
+    save_screenshot
 
-#     # DEBUG
-#     save_screenshot
-
-#     # VERIFY
-#     expect(page).to have_css 'article.product', count: 10
-#   end
-# end
+    # VERIFY
+    expect(page).to have_selector 'section.products-show'
+    expect(page).to have_text p.name
+  end
+end
